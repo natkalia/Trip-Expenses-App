3781f0eac215c8b90ab529f3722f2039852f5304
anna <annapiwonska1@gmail.com>
2019-12-29 18:09:46 +0100

    Add mongoose schema for user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,33 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const Joi = require('@hapi/joi');
+
+const userSchema = new mongoose.Schema({
+  username:{
+    type: String,
+    required: [true, 'Username is required'],
+    minlength: [3, 'Username must be at least 3 characters long'],
+    maxlength: [20, 'Username must be at most 20 characters long']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Not a valid email address'
+    ],
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password must be at least 8 characters long'],
+    maxlength: 500,
+    trim: true
+  },
+})
+
+const User = mongoose.model('User', userSchema);
 
 exports.User = User;
 exports.validate = validateUser;
