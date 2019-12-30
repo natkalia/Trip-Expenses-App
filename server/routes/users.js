85c1f54ecc80d5e748a000bdbd0cfd2e72603299
anna <annapiwonska1@gmail.com>
2019-12-30 22:06:22 +0100

    Add GET route for user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,15 @@
+const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+
+// User GET route
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select('name email trips');
+    res.json(user)
+  } catch (error) {
+    res.status(400).json('Error: ' + error)
+  }
+});
 
 module.exports = router;
