3074d21414667ece45c1cf856b7b1d4d16fab33b
unknown <natalia.banasiewicz@wp.pl>
2020-01-03 11:26:18 +0100

    Move styles from AddExpense to separate file

--- client/src/components/AddExpense.js
+++ client/src/components/AddExpense.js
@@ -1,62 +1,8 @@
 import React, { Component } from 'react'; 
-import styled from 'styled-components';
-import Button from './Button';
 import axios from 'axios';
+import Button from './Button';
+import { Title, Form, Label, Input, Wrapper } from './styled';
 import Select from 'react-select';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  max-width: 400px;
-  margin: 0 auto;
-`
-const Title = styled.h2`
-  font-family: 'Roboto', sans-serif;
-  font-weight: normal;
-  font-size: 24px;
-  color: #4B52EE;
-  margin: 10px auto;
-`;
-
-const Form = styled.form`
-  width: 80%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: left;
-  padding: 10px;
-  margin: 10px auto;
-`;
-
-const Label = styled.label`
-  font-family: 'Roboto', sans-serif;
-  font-weight: normal;
-  font-size: 18px;
-  color: #000;
-  width: 100%;
-  margin: 10px auto 0;
-`;
-
-const Input = styled.input`
-  font-family: 'Roboto', sans-serif;
-  font-weight: normal;
-  font-size: 18px;
-  color: #000;
-  display: block;
-  border: 1px solid #000;
-  padding: 4px;
-  border-radius: 3px;
-  min-height: 26px;
-  width: 100%;
-  margin: 0 auto;
-  &::placeholder{
-    font-family: 'Roboto', sans-serif;
-    font-size: 18px;
-    color: #999;
-  }
-`;
 
 class AddExpense extends Component {
 
