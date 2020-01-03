c9a26beada84aec7c9c259a06f97896f93d606b4
Agata Ceglarz <pazagata@gmail.com>
2020-01-03 21:03:45 +0100

    Update: Add links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,8 +4,26 @@ import React from 'react';
 const Footer = () => {
   return (
     <div>
-      <p>Trip Expenses App was created by Warsaw Group during Coderscamp 2019/2020.</p>
-      <p>More info and source code here.</p>
+      <p>
+        Trip Expenses App was created by Warsaw Group during{' '}
+        <a
+          href="https://coderscamp.edu.pl/"
+          title="See CodersCamp Page"
+        >
+          Coderscamp 2019/2020
+        </a>
+        .
+      </p>
+      <p>
+        More info and source code{' '}
+        <a
+          href="https://github.com/dobrzyckahanna/TravelPlanner"
+          title="See on Github"
+        >
+          here
+        </a>
+        .
+      </p>
       <p>©All Rights Reserved, 2020</p>
     </div>
   )
