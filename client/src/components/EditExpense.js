199b3af1b86925e14122a20061749336da570b56
unknown <natalia.banasiewicz@wp.pl>
2020-01-04 17:31:27 +0100

    Update EditExpense form to work properly with state and default values

--- client/src/components/EditExpense.js
+++ client/src/components/EditExpense.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'; 
 import axios from 'axios';
+import ContentWrapper from './ContentWrapper';
 import Button from './Button';
-import { Title, Form, Label, Input, Wrapper, customStyleSelect } from './styled';
+import { Form, Label, Input, customStyleSelect } from './styled';
 import Select from 'react-select';
 
 class EditExpense extends Component {
@@ -9,13 +10,21 @@ class EditExpense extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tripId: "5e0dd4fa618f3e1f10d4db80",
-      expenseId: "5e0e10274f0e0f24a0e1ec1c",
+      tripId: "5e0dd4fa618f3e1f10d4db80", // temporary
+      expenseId: "5e0e10274f0e0f24a0e1ec1c", // temporary
       expenseName: "", 
-      category: "",
-      cost: "",
-      currency: "",
-      optionsCurrencies: [
+      expenseCategory: 
+      {
+        value: "", 
+        label: ""
+      },
+      expenseCost: "",
+      expenseCurrency: 
+      {
+        value: "", 
+        label: ""
+      },
+      tripCurrencies: [
         { value: 'PLN', label: 'PLN' },
         { value: 'USD', label: 'USD' },
         { value: 'EUR', label: 'EUR' },
@@ -25,99 +34,178 @@ class EditExpense extends Component {
         { value: 'CAD', label: 'CAD' },
         { value: 'CHF', label: 'CHF' }
       ],
-      optionsCategories: [
-        { value: 'food', label: 'food' },
-        { value: 'travel', label: 'travel' },
-        { value: 'tickets', label: 'tickets' },
-        { value: 'accomodation', label: 'accomodation' },
-        { value: 'health and insurance', label: 'health and insurance' },
-        { value: 'other', label: 'other' }
-      ]
+      tripCategories: []
     }
   }
 
   onInputChange = (e) => {
-    const target = e.target;
-    const value = target.value; 
-    const name = target.name;
+    const value = e.target.value; 
+    const name = e.target.name;
     this.setState({
       [name]: value
     });
   }
 
   onSelectCurrencyChange = (optionsObject) => {
-    const value = optionsObject.value; 
+
     this.setState({
-      currency: value
+      expenseCurrency: optionsObject
     });
   }
 
   onSelectCategoryChange = (optionsObject) => {
-    const value = optionsObject.value; 
+
     this.setState({
-      category: value
+      expenseCategory: optionsObject
     });
   }
 
-  onEditSubmit = (e) => { 
+  onEditSubmit = async (e) => { 
     e.preventDefault();
     const expense = {
       name: this.state.expenseName,
-      category: this.state.category,
-      cost: this.state.cost,
-      currency: this.state.currency
+      category: this.state.expenseCategory.value,
+      cost: this.state.expenseCost,
+      currency: this.state.expenseCurrency.value
     }
-
-    axios.put(`http://localhost:3000/api/trips/${this.state.tripId}/expenses/${this.state.expenseId}`, expense)
-      .then(res => console.log(res.data));
-
-    // reset inputs to blank to start over again after form submit
-    this.setState({ expenseName: "", category: "", currency: "", cost: "" })
-
-    console.log(this.state.expenseName)
-  };
-
-
-  onDeleteSubmit = (e) => {
-    e.preventDefault();
-    if(window.confirm("Are you sure you want to delete this expense?")) {
-    axios.delete(`http://localhost:3000/api/trips/${this.state.tripId}/expenses/${this.state.expenseId}`)
-    .then(res => console.log(res.data));
-    } else return;
-  };
-
-  componentDidMount () {
-    axios.get(`http://localhost:3000/api/trips/${this.state.tripId}/expenses/${this.state.expenseId}`)    
-      .then(res => this.setState({ 
-        expenseName: res.data.expense.name,
-        category: res.data.expense.category,
-        cost: res.data.expense.cost,
-        currency: res.data.expense.currency
-    }));
-    console.log(this.state); // issue with lifecycle methods and showing data in currency and category
+    console.log(expense);
+      await axios.put(`http://localhost:3000/api/trips/${this.state.tripId}/expenses/${this.state.expenseId}`, expense);
+      // not sure if this cleanup of state is necessary as we redirect user to other path
+      try {
+        this.setState({ 
+          expenseName: "", 
+          expenseCategory: {
+            value: "", 
+            label: ""
+          },
+          expenseCurrency: {
+            value: "", 
+            label: ""
+          }, 
+          expenseCost: ""
+        });
+        confirm("Expense was succesfully edited and saved!"); // validation should be added to check this
+        window.location=`/trips/single/${this.state.tripId}`; // redirect to single trip after editing expense
+      } catch (error) {
+        this.setState({ error: 'Error' });
+      }
   }
 
-  componentDidUpdate () {
-    console.log(this.state); // issue with lifecycle methods and showing data in currency and category
+  onDeleteSubmit = async (e) => {
+    e.preventDefault();
+    if(window.confirm("Are you sure you want to delete this expense?")) {
+
+      await axios.delete(`http://localhost:3000/api/trips/${this.state.tripId}/expenses/${this.state.expenseId}`);
+      // not sure if this cleanup of state is necessary as we redirect user to other path
+      try {
+        this.setState({ 
+          expenseName: "", 
+          expenseCategory: {
+            value: "", 
+            label: ""
+          },
+          expenseCurrency: {
+            value: "", 
+            label: ""
+          }, 
+          expenseCost: ""
+        });
+        confirm("Expense is deleted!"); // validation should be added to check this
+        window.location=`/trips/single/${this.state.tripId}`; // redirect to single trip after deleting expense
+      } catch (error) {
+        this.setState({ error: 'Error' });
+      }
+    } else return;
+  }
+
+  getCategoriesFromTrip = async () => {
+    const res = await axios.get(`http://localhost:3000/api/trips/${this.state.tripId}`);
+    try {
+      const sanitizedArrayCategories = res.data.categories.map(option => ({ value: option, label: option }));
+      this.setState({
+        tripCategories: sanitizedArrayCategories,
+      });
+    } catch (error) {
+      this.setState({ error: 'Error' });
+    }
+  }
+
+  getExpenseData = async () => {
+    const res = await axios.get(`http://localhost:3000/api/trips/${this.state.tripId}/expenses/${this.state.expenseId}`);
+    try {
+      this.setState({
+        expenseName: res.data.expense.name,
+        expenseCost: res.data.expense.cost,
+        expenseCurrency: { value: res.data.expense.currency, label: res.data.expense.currency },
+        expenseCategory: { value: res.data.expense.category, label: res.data.expense.category }
+      });
+    } catch (error) {
+      this.setState({ error: 'Error' });
+    }
+  }
+
+  componentDidMount () {
+    // refactor to make one get request? use sth to includes ${this.state.expenseId} and then res.data.foundExpense.currency etc?
+    this.getCategoriesFromTrip();
+    this.getExpenseData();
   }
  
   render() {
     return (
-      <Wrapper>
-        <Title>Edit Expense</Title>
+      <ContentWrapper title="Edit Expense">
+
         <Form onSubmit={this.onEditSubmit}>
 
-          <Label htmlFor="name-exp-add">Name (3-30 characters):</Label>
-          <Input minlength="3" maxlength="30" type="text" name="expenseName" id="name-exp-add" placeholder="Name" required onChange={this.onInputChange} value={this.state.expenseName}/>
+          <Label htmlFor="expenseName-edit">Name (3-30 characters):</Label>
+          <Input 
+            minlength="3" 
+            maxlength="30" 
+            type="text" 
+            name="expenseName" 
+            id="expenseName-edit"
+            placeholder="Name"
+            required 
+            onChange={this.onInputChange} 
+            value={this.state.expenseName}
+          />
 
-          <Label htmlFor="amount-add">Cost (0-10000):</Label>
-          <Input min="0" max="10000" type="number" name="cost" id="cost-add" placeholder="Cost amount" required onChange={this.onInputChange} value={this.state.cost}/>
+          <Label htmlFor="expenseCost-edit">Cost (0-10000):</Label>
+          <Input 
+            min="0" 
+            max="10000" 
+            type="number" 
+            name="expenseCost" 
+            id="expenseCost-edit" 
+            placeholder="Cost amount" 
+            required 
+            onChange={this.onInputChange} 
+            value={this.state.expenseCost}
+          />
 
-          <Label htmlFor="currency-edit">Currency:</Label>
-          <Select styles={customStyleSelect} options={this.state.optionsCurrencies} type="text" name="currency" id="currency-edit" placeholder="Currency" required onChange={this.onSelectCurrencyChange} value={this.state.currency.value}/>
+          <Label htmlFor="expenseCurrency-edit">Currency:</Label>
+          <Select 
+            styles={customStyleSelect} 
+            options={this.state.tripCurrencies} 
+            type="text" 
+            name="expenseCurrency" 
+            id="expenseCurrency-edit" 
+            placeholder="Currency" 
+            required onChange={this.onSelectCurrencyChange} 
+            value={this.state.expenseCurrency}
+          />
 
-          <Label htmlFor="category-edit">Category:</Label>
-          <Select styles={customStyleSelect} options={this.state.optionsCategories} type="text" name="category" id="category-edit" placeholder="Category" required onChange={this.onSelectCategoryChange} value={this.state.category.value}/>
+          <Label htmlFor="expenseCategory-edit">Category:</Label>
+          <Select 
+            styles={customStyleSelect} 
+            options={this.state.tripCategories} 
+            type="text" 
+            name="expenseCategory" 
+            id="expenseCategory-edit" 
+            placeholder="Category" 
+            required 
+            onChange={this.onSelectCategoryChange} 
+            value={this.state.expenseCategory}
+          />
 
           <Button textOnButton="Edit" btnColor="#2EC66D" btnBorder="none"/> 
 
@@ -127,7 +215,7 @@ class EditExpense extends Component {
           <Button textOnButton="Delete" btnColor="#DC3545" btnBorder="none"/> 
         </Form>
 
-      </Wrapper>
+      </ContentWrapper>
     )
   }
 } 
