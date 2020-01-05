fdf03d61962fe040d3b4f27c9ad64a8b5933b0dd
anna <annapiwonska1@gmail.com>
2020-01-05 18:41:07 +0100

    Add authentication middleware function checkAuthenticated

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,2 +1,20 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
+
+export const checkAuthenticated = async (req, res, next) => {
+  const token = req.headers("x-auth-token") || null;
+  if (!token) return res.status(401).send("Access denied. No token provided");
+
+  try {
+    const decrypt = jwt.verify(token, config.get('db.jwtPrivateKey'));
+    req.user = decrypt;
+    next();
+  } catch(err) {
+    return res.status(400).send("Invalid token");
+  }
+}
+
+// export const checkAuthorized = async (req, res, next) => {
+  
+// }
+
--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ const mongoose = require('mongoose');
 const tripsRouter = require('./routes/trips');
 const usersRouter = require('./routes/users');
 
+const checkAuthenticated = require('./middleware/auth')
+
 const app = express();
 
 if (!config.get('db.jwtPrivateKey')) {
@@ -42,7 +44,7 @@ if (app.get('env') === 'development') {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/trips', tripsRouter);
+app.use('/api/trips', checkAuthenticated, tripsRouter);
 app.use('/api/users', usersRouter);
 
 const port = process.env.PORT || 5000;
