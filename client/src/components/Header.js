137a6f44ed1981ba95a463d1b52428239581e895
Agata Ceglarz <pazagata@gmail.com>
2020-01-05 11:26:51 +0100

    Update: Add dropdown

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,12 +8,21 @@ import { theme } from '../utils/theme';
 const HeaderWrapper = styled.div`
   background-color: ${theme.colors.whiteOverlay};
   width: 100%;
-  padding: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 0px 4px 4px ${theme.colors.bgOverlay};
+`;
+
+const TopLabel = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  box-shadow: 0px 4px 4px ${theme.colors.bgOverlay};
+  padding: 15px 25px;
+  box-shadow: 0px 0px 4px ${theme.colors.bgOverlay};
 `;
 
 const AppName = styled.div`
@@ -35,21 +44,32 @@ const H1 = styled.h1`
   font-family: ${theme.fonts.special};
   font-weight: normal;
   color: ${theme.colors.neutralDark};
+  margin: 0;
+
+`;
+
+const Nav = styled.nav`
+  width: 100%;
 `;
 
 const Ul = styled.ul`
+  width: 100%;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  margin: 0;
+  padding: 15px 25px;
 `;
 
 const NavForNotLoggedIn = styled(Ul)`
+  display: ${props => props.showMenu ? 'flex' : 'none'};
+  justify-content: flex-end;
 
 `;
 
 const NavForLoggedIn = styled(Ul)`
-  /* display: none; */
+  justify-content: space-between;
+  display: ${props => props.showMenu ? 'flex' : 'none'};
 `;
 
 const Li = styled.li`
@@ -67,33 +87,42 @@ class Header extends Component {
   constructor(props){
     super(props);
     this.state = {
-      menuOpened: false
+      menuOpened: false,
+      isLoggedIn: false
     }
   }
   
   toggleMenu = () => {
-    console.log('ok');
+    this.setState((prevState, props) => ({
+      menuOpened: !prevState.menuOpened
+    }))
   }
 
   render() {
     return (
       <HeaderWrapper>
-        <AppName>
-          <Logo></Logo>
-          <H1>Trip Expenses</H1>
-        </AppName>
-        <nav>
+        <TopLabel>
+          <AppName>
+            <Logo></Logo>
+            <H1>Trip Expenses</H1>
+          </AppName>
           <div onClick={this.toggleMenu}>otworz</div>
-          <NavForNotLoggedIn>
+        </TopLabel>
+        <Nav>
+          <NavForNotLoggedIn
+            showMenu={this.state.menuOpened && !this.state.isLoggedIn}
+          >
             <Li><StyledLink to={'/users/login'}>Login</StyledLink></Li>
             <Li><StyledLink to={'/users/register'}>Sign Up</StyledLink></Li>
           </NavForNotLoggedIn>
-          <NavForLoggedIn>
+          <NavForLoggedIn
+            showMenu={this.state.menuOpened && this.state.isLoggedIn}
+          >
             <Li><StyledLink to={'/trips/all'}>Trips</StyledLink></Li>
             <Li><StyledLink to={'/users/profile'}>Settings</StyledLink></Li>
             <Li>Log out</Li>
           </NavForLoggedIn>
-        </nav>
+        </Nav>
       </HeaderWrapper>
     )
   }
