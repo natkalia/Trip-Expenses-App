a6c6d68c70e93f4219076fced0c648cb843eeb19
anna <annapiwonska1@gmail.com>
2020-01-05 12:11:11 +0100

    Create SingleTrip component and modify styled.js

--- client/src/components/SingleTrip.js
+++ client/src/components/SingleTrip.js
@@ -1,5 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import moment from 'moment';
+import axios from 'axios';
+import {
+  ContentWrapper,
+  HeaderWrapper,
+  InnerContainer,
+  ParagraphAlignedCenter,
+  LinkButtonBig,
+} from './styled';
 
-const SingleTrip = () => <div>tu będzie SingleTrip</div>
+
+class SingleTrip extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      startDate: "",
+      description: undefined,
+      isTripFinished: false
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3000/api/trips/${this.props.match.params.id}`)
+      .then(res => this.setState({
+        id: res.data._id,
+        name: res.data.name,
+        startDate: new Date(res.data.startDate),
+        description: res.data.description,
+        isTripFinished: res.data.isTripFinished
+      }));
+  }
+  
+  render() {
+    return (
+      <ContentWrapper>
+
+        <HeaderWrapper>
+          <InnerContainer>
+            <ParagraphAlignedCenter>
+              <b>Start Date:</b> &nbsp;
+              {this.state.startDate ? moment(this.state.startDate).format('YYYY-MM-DD'): ""}
+            </ParagraphAlignedCenter>
+            <ParagraphAlignedCenter> 
+              {this.state.description}
+            </ParagraphAlignedCenter>
+          </InnerContainer>          
+        </HeaderWrapper>
+
+        <LinkButtonBig to={`/trips/${this.props.match.params.id}/expenses/add`} color="green">Add Expense</LinkButtonBig>
+        <LinkButtonBig to={`/trips/summary/${this.props.match.params.id}`} color="greenOutline">Summary</LinkButtonBig>
+      </ContentWrapper>
+    )
+  }
+}
 
 export default SingleTrip;
--- client/src/components/styled.js
+++ client/src/components/styled.js
@@ -1,10 +1,81 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { theme } from '../utils/theme';
 
 export const H2 = styled.h2 `
   font-size: 24px;
-  font-weight:400;
+  font-weight: 400;
 `
+export const ParagraphAlignedCenter = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  text-align: center;
+  margin: 0 auto 10px;`
+
+export const ContentWrapper = styled.div `
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  width:100%;
+  background-color: rgba(255,255,255,0.85);
+  margin: 0 auto 40px;
+  padding-bottom:20px;
+`;
+
+export const HeaderWrapper = styled.div `
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  justify-content:center;
+  min-height: 70px;
+  padding: 20px 0;
+  margin-bottom: 30px;
+  background-color: rgba(255,255,255,0.85);
+  box-shadow: 0 4px 4px ${theme.colors.neutralExtraLight};
+`;
+
+export const InnerContainer = styled.div`
+  width: 90%;
+  margin-bottom: 10px;
+`
+
+export const LinkButtonBig = styled(Link)`
+  width: 80%;
+  min-height: 40px;
+  margin: 0 auto 20px;
+  padding: 6.5px;
+  font-size: 20px;
+  font-weight: 400;
+  text-decoration: none;
+  text-align: center;
+  background-color: ${props => (
+    props.color === "green" ? theme.colors.btnMain 
+    : theme.colors.white 
+    )};
+  color: ${props => (
+    props.color === "green" ? theme.colors.white 
+    : props.color === "greenOutline" ? theme.colors.btnMain
+    : theme.colors.neutralMidDark
+    )};
+  border: 1px solid ${props => (
+    props.color === "green" ? theme.colors.btnMain
+    : props.color === "greenOutline" ? theme.colors.btnMain
+    : theme.colors.neutralMidDark
+    )};
+  border-radius: 5px;  
+  box-shadow: 0 0 4px ${theme.colors.neutralLight};
+  cursor: pointer;
+  transition: .3s;
+
+  &:hover{
+    background-color: ${props => (
+      props.green && '#15AD54'
+      )};
+      font-weight: 600;
+      box-shadow: 0 0 4px 
+  } 
+`;
+
 // Forms
 
 export const Form = styled.form`
@@ -30,16 +101,16 @@ export const Input = styled.input`
   padding: 5px;
   font-size: 16px;
   color: ${theme.colors.neutralDark};
-  border: 1px solid ${theme.colors.neutralLight};
+  border: 1px solid ${theme.colors.neutralExtraLight};
   border-radius: 5px;  
-  box-shadow: 0 0 4 ${theme.colors.neutralDark};  
+  box-shadow: 0 0 4px ${theme.colors.neutralExtraLight};  
   &::placeholder{
     font-size: 16px;
     color: #999;
   }
   &:focus{ 
     outline: none;
-    /* box-shadow: 0 0 4 ${theme.colors.neutralMidDark}; */
+    box-shadow: 0 0 4px ${theme.colors.neutralMidLight}; 
   }
 `;
 
