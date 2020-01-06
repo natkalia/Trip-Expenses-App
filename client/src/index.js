bdaa948a0701914e1a2a995b61aea4deed6cca64
Agata Ceglarz <pazagata@gmail.com>
2020-01-06 18:07:19 +0100

    Update: Add redux persist

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from "./components/App";
 import exampleReducer from './reducers/exampleReducer';
 
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, exampleReducer)
+
 const store = createStore(
-  exampleReducer,
+  persistedReducer,
   window.devToolsExtension && window.devToolsExtension()
 );
 
+let persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App/>    
+    <PersistGate loading={null} persistor={persistor}>
+      <App/>
+    </PersistGate> 
   </Provider>
   , document.querySelector("#root"));
