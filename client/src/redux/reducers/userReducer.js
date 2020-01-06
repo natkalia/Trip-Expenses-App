30db1e79a85fcd692df3c939a6dabb596503b6c2
Agata Ceglarz <pazagata@gmail.com>
2020-01-06 15:42:06 +0100

    Update: Add logout to reducer

--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -1,5 +1,6 @@
 import {
-  SET_LOGGED_IN
+  SET_LOGGED_IN,
+  SET_LOGGED_OUT
 } from '../actions/userActions';
 
 
@@ -10,6 +11,8 @@ const initialState = {
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_LOGGED_IN:
+      return Object.assign({}, state,action.payload)
+    case SET_LOGGED_OUT:
       return Object.assign({}, state,action.payload)
     default:
       return state
