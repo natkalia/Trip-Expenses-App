8ba775baefc6a4f0d3601657297590ce751801b6
anna <annapiwonska1@gmail.com>
2020-01-07 18:02:44 +0100

    Create HomePage component and add image

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { H2, ContentWrapper } from './styled.js';
+import HomePageImage from '../images/map-with-pins.png';
 
-const HomePage = () => <div>tu będzie HomePage</div>
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+const InnerHeaderContainer = styled.div`
+  flex-basis: 1;
+`
 
+
+class HomePage extends Component {
+  render() {
+    return(
+      <ContentWrapper>
+        <HeaderContainer>
+          <img src={HomePageImage} alt="Map image"/>
+          <InnerHeaderContainer>
+            <H2>We help you to manage your travel costs!</H2>
+          </InnerHeaderContainer>          
+        </HeaderContainer>
+
+      </ContentWrapper>
+    )
+  }
+}
 export default HomePage;
