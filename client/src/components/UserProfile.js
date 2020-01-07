dbed0b53678ed30610e5a9b5fd47392c9cf69cf3
anna <annapiwonska1@gmail.com>
2020-01-08 00:08:16 +0100

    Create UserProfile component

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,5 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { theme } from '../utils/theme';
+import ContentWrapper from './ContentWrapper';
+import { H2, H3, InnerContainer, LinkButtonBig, LinkButtonSmall } from './styled';
 
-const UserProfile = () => <div>tu będzie UserProfile</div>
+const SectionTitle = styled(H3)`
+  &::after{
+    content: "";
+    display: block;
+    height:2px;
+    width:70%;
+    margin: 15px 0 30px;
+    background-color: ${theme.colors.neutralMidLight};
+  }
+`;
+
+const Button = styled(LinkButtonSmall)`
+  width: 200px;
+  margin: 0 auto 20px 0;
+` 
+
+const ParagraphAlignedLeft = styled.p `
+  font-size: 16px;
+  font-weight: 400;
+  text-align: left;
+  margin: 0 0 8px 0;
+`;
+
+const Section = styled.div`
+  margin-bottom: 30px;
+`;
+
+const DataGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  ${theme.media.tablet} {
+    flex-direction:column;
+  }
+`;
+
+const LabelData = styled.div`
+  width: 90px;
+  margin-right: 20px;
+`
+
+class UserProfile extends Component {
+  render() {
+    return (
+      <ContentWrapper title="Settings">
+        <InnerContainer>
+          <Section>          
+            <SectionTitle>Your Profile Information</SectionTitle>
+            <DataGroup>
+              <LabelData>User Name:</LabelData>
+              <ParagraphAlignedLeft>Anna Piwonska</ParagraphAlignedLeft>
+            </DataGroup>
+            <DataGroup>
+              <LabelData>Email:</LabelData>
+              <ParagraphAlignedLeft>anna@test.pl</ParagraphAlignedLeft>
+            </DataGroup>      
+          </Section>
+          <Section>
+            <SectionTitle>Category settings:</SectionTitle>
+            <Button to={`/`} color="disabled">Manage Categories</Button>
+          </Section>
+          <Section>
+            <SectionTitle>Currency settings:</SectionTitle>
+            <Button to={`/`} color="disabled">Manage Currency</Button>
+          </Section>
+        </InnerContainer>
+      </ContentWrapper>
+    )
+  }
+}
 
 export default UserProfile;
