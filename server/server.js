a8fcbd4e5b06506ddb6b24e3796635a0acf2febf
unknown <natalia.banasiewicz@wp.pl>
2020-01-08 14:46:51 +0100

    Update path to static file

--- server/server.js
+++ server/server.js
@@ -53,11 +53,13 @@ app.use('/api/currencies', currencyRouter);
 
 // Deployment - serve static
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     // res.sendFile(path.join(__dirname + "../client/build/index.html"));
-  }); 
+  // }); 
 }
 
 const port = process.env.PORT || 5000;
